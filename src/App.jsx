c24a397761888342a91e0529344d265ca1a029e5
wahab3913 <104102500+wahab3913@users.noreply.github.com>
2023-05-10 18:37:13 +0500

    all data in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Details from "./Component/Details";
 import Home from "./Component/Home";
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
+  const [allData, setAllData] = useState([]);
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/details/:id" element={<Details />} />
+          <Route
+            path="/"
+            element={<Home allData={allData} setAllData={setAllData} />}
+          />
+          <Route path="/details/:id" element={<Details allData={allData} />} />
         </Routes>
       </BrowserRouter>
     </>
